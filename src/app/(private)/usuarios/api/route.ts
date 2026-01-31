a71b8d2117f19936b710631bda2c3b64236e73f3
Jean Victor <jeanvictormachado3@gmail.com>
2026-01-30 21:46:14 -0300

    fix: check correct user_metadata

--- src/app/(private)/usuarios/api/route.ts
+++ src/app/(private)/usuarios/api/route.ts
@@ -14,7 +14,7 @@ export async function GET (request: Request) {
         return;
     }
 
-    const users = usersData.users.filter(user => user.user_metadata.data.polo_id ).filter((usuario) => {
+    const users = usersData.users.filter(user => user.user_metadata.polo_id ).filter((usuario) => {
         if (!queryParam && !poloId) return true;
         if(queryParam && !poloId) {
             return usuario.user_metadata.name.toLowerCase().includes(queryParam?.toLowerCase()) || 
